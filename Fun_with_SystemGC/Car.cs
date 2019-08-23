c56c572f0b3a1855b9affadd6f505e9c73f59420
Толстов Александр <alextav_85@mail.ru>
2019-08-23 09:14:30 +0600

    Add project files.

---
+++ Fun_with_SystemGC/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun_with_SystemGC
+{
+    class Car
+    {
+        public delegate void CarEngineHandler(string msg);
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+        private bool carIsDead;
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+        //public delegate void CarEngineHandler(string msgForCaller);
+        private CarEngineHandler listOfHandlers;
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers += methodToCall;
+        }
+        public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers -= methodToCall;
+        }
+        public void Accelerat(int delta)
+        {
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded("Sorry? this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if(10 == (MaxSpeed - CurrentSpeed) && AboutToBlow != null)
+                {
+                    AboutToBlow("Careful buddy! Gonna blow!");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ Fun_with_SystemGC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun_with_SystemGC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********* Fun with System.GC *********");
+            Console.WriteLine("Estimated bytes on heap: {0}", GC.GetTotalMemory(false));
+
+            Console.WriteLine("This OS has {0} object generations.\n", (GC.MaxGeneration + 1));
+            Car refToMyCar = new Car("Zippy", 100, 200);
+            Console.WriteLine(refToMyCar.ToString());
+            Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+        }
+    }
+}
